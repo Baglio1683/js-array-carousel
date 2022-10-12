5e00640b4c2bf78df84bc687a4d0a62265ecad8e
MyUsername <alberto.baglivi@gmail.com>
2022-10-12 17:31:02 +0200

    Add First javascript setting

--- js/script.js
+++ js/script.js
@@ -1,2 +1,54 @@
 const listItem = ["img/01.jpg","img/02.jpg", "img/03.jpg","img/04.jpg","img/05.jpg"]
 
+const rowImages = document.querySelector(".row")
+
+for(let i = 0 ; i < listItem.length ; i++){
+
+   const imageRow = document.createElement("img")    
+   imageRow.src = listItem[i]
+   imageRow.classList.add("ms_item")
+  
+   if(i==0){
+       imageRow.classList.add("ms_active")
+    }
+
+    rowImages.append(imageRow)
+
+}
+
+
+
+const items = document.getElementsByClassName("ms_item")
+console.log(items)
+
+let posImage = 0 ; 
+
+const upAngle = document.querySelector(".ms_angleup")
+
+const downAngle = document.querySelector(".ms_angledown")
+
+
+
+upAngle.addEventListener("click", function(){
+
+    items[posImage].classList.remove("ms_active")
+
+    posImage++
+
+    items[posImage].classList.add("ms_active")
+
+}) 
+
+
+downAngle.addEventListener("click", function(){
+
+    items[posImage].classList.remove("ms_active")
+
+    posImage--
+
+    items[posImage].classList.add("ms_active")
+
+}) 
+
+
+
